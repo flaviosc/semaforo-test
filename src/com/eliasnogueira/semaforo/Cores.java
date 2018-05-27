de40fdb8c5b8e4709d2603ff2fb76db8052fe333
Flávio Santana <flaviosantana.c@gmail.com>
2018-05-26 23:04:14 -0300

    modificação retorno classe semáforo

--- src/com/eliasnogueira/semaforo/Cores.java
+++ src/com/eliasnogueira/semaforo/Cores.java
@@ -2,7 +2,7 @@ package com.eliasnogueira.semaforo;
 
 public interface Cores {
 
-	public static final String VERDE = "AZUL";
+	public static final String VERDE = "VERDE";
 	public static final String AMARELO = "AMARELO";
 	public static final String VERMELHO = "VERMELHO";
 }
